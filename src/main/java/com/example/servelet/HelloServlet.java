58b24a407d2c9d614b63c400c45899ec4a0f1def
tmtri3102 <tmtri310251@gmail.com>
2024-11-27 10:00:00 +0700

    first commit

---
+++ src/main/java/com/example/servelet/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.servelet;
+
+import java.io.*;
+import java.util.Date;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
